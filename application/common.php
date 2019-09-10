312b4e24116e3797ee51f2483e6b84a119cc771d
admin <1428768840@qq.com>
2019-09-10 14:38:47 +0800

    接口修改

--- application/common.php
+++ application/common.php
@@ -11,71 +11,40 @@
 
 // 应用公共文件
 
-use OSS\OssClient;
-use OSS\Core\OssException;
-use Aliyun\Core\Config;
-use Aliyun\Core\Profile\DefaultProfile;
-use Aliyun\Core\DefaultAcsClient;
-use Aliyun\Api\Sms\Request\V20170525\SendSmsRequest;
+use AlibabaCloud\Client\AlibabaCloud;
+use AlibabaCloud\Client\Exception\ClientException;
+use AlibabaCloud\Client\Exception\ServerException;
 
 /**
- * 短信发送
- * @param $to    接收人 （多个接收人用逗号隔开）
- * @param $model    短信模板ID
- * @param $code   短信验证码
+ * 阿里云短信服务接口调用
+ * @param $action    接口类型 
+ * @param $data    携带参数（array()）
  * @return json
  * @
  */
-function send_sms($to, $model, $code)
-{
-    require_once '../extend/alisms/vendor/autoload.php';
-    Config::load(); //加载区域结点配置
-    $config = '根据你的实际情况读取配置文件或读取数据库，本项目是将配置写入数据库实现';
-    $accessKeyId = 'LTAI4FoHccrYsh9cWGHqULss';
-    $accessKeySecret = 'KEK8lwdg1YhXLFNDQtL5AWOxhsx6tK';
-    $templateParam = $code;
-    //短信签名
-    $signName = '畅通网络科技有限公司';
-    //短信模板ID
-    switch ($model) {
-        case 1:
-            $templateCode = 'SMS_173478946'; // 发送短信验证码模板
-            break;
-        case 2:
-			$templateCode = 'SMS_173473936'; // 发送短信通知模板
-            break;
-		case 3:
-			$templateCode = $config['model_code_reset']; // 重置密码短信验证码模板
-            break;
+
+function aliyun_api($action,$data){
+    AlibabaCloud::accessKeyClient($accessKeyId = 'LTAI4FoHccrYsh9cWGHqULss',
+            $accessKeySecret = 'KEK8lwdg1YhXLFNDQtL5AWOxhsx6tK')
+                    ->regionId('cn-hangzhou') // replace regionId as you need
+                    ->asDefaultClient();
+
+    try {
+        $result = AlibabaCloud::rpc()
+                    ->product('Dysmsapi')
+                    // ->scheme('https') // https | http
+                    ->version('2017-05-25')
+                    ->action($action)
+                    ->method('POST')
+                    ->host('dysmsapi.aliyuncs.com')
+                    ->options([
+                                'query' => $data,
+                            ])
+                    ->request();
+        return $result->toArray();
+    } catch (ClientException $e) {
+        echo $e->getErrorMessage() . PHP_EOL;
+    } catch (ServerException $e) {
+        echo $e->getErrorMessage() . PHP_EOL;
     }
-    //短信API产品名（短信产品名固定，无需修改）
-    $product = "Dysmsapi";
-    //短信API产品域名（接口地址固定，无需修改）
-    $domain = "dysmsapi.aliyuncs.com";
-    //暂时不支持多Region（目前仅支持cn-hangzhou请勿修改）
-    $region = "cn-hangzhou";
-    // 初始化用户Profile实例
-    $profile = DefaultProfile::getProfile($region, $accessKeyId, $accessKeySecret);
-    // 增加服务结点
-    DefaultProfile::addEndpoint("cn-hangzhou", "cn-hangzhou", $product, $domain);
-    // 初始化AcsClient用于发起请求
-    $acsClient = new DefaultAcsClient($profile);
-    // 初始化SendSmsRequest实例用于设置发送短信的参数
-    $request = new SendSmsRequest();
-    // 必填，设置雉短信接收号码
-    $request->setPhoneNumbers($to);
-    // 必填，设置签名名称
-    $request->setSignName($signName);
-    // 必填，设置模板CODE
-    $request->setTemplateCode($templateCode);
-    // 可选，设置模板参数
-    if ($templateParam) {
-        $request->setTemplateParam(json_encode($templateParam));
-    }
-    //发起访问请求
-    $acsResponse = $acsClient->getAcsResponse($request);
-    //返回请求结果
-    $result = json_decode(json_encode($acsResponse), true);
-    // 具体返回值参考文档：https://help.aliyun.com/document_detail/55451.html?spm=a2c4g.11186623.6.563.YSe8FK
-    return $result;
 }
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,15 +3,33 @@ namespace app\index\controller;
 
 class Index
 {
-    public function index()
+	public function index(){
+
+	}
+
+	//发送信息
+    public function send()
     {
-		$code  = rand(100000,999999);
-		//$phone = '13096900712,13468678637';
-		$send = send_sms($phone, 1, ['code'=>$code]);
-		if($send['Message'] != 'OK'){
-			dump($send);
-		}else{
-			echo '发送成功';
-		}
-    }
+    	$data=[
+    		'RegionId' => "cn-hangzhou",
+	        //'PhoneNumbers' => "18710359268,13096900712,18710376114",
+	        'SignName' => "畅通网络科技有限公司",
+	        'TemplateCode' => "SMS_173473936",
+    	];
+		aliyun_api('SendSms',$data);
+	}
+
+	//获取短信发送记录和发送状态
+	public function send_details(){
+		echo '<pre>';
+		$data= [
+			'RegionId' => "cn-hangzhou",
+            'PhoneNumber' => "13096900712",
+            'SendDate' => "20190910",
+            'PageSize' => "50",
+            'CurrentPage' => "1",
+		];
+		$result = aliyun_api('QuerySendDetails',$data);
+		dump($result);
+	}
 }
